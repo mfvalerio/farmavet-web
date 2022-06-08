c9ce1968ae59a127a2b34bed9afa42a0a09f3626
mfvalerio <mariana.dfvalerio@gmail.com>
2022-06-08 12:01:06 -0300

    adicionando https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://farmavet-api.herokuapp.com/'
+  apiUrl: 'https://farmavet-api.herokuapp.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://farmavet-api.herokuapp.com/'
+  apiUrl: 'https://farmavet-api.herokuapp.com/'
 };
 
 /*
